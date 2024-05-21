bc90ce29b84272833b78865064c1a429983e35a9
EscobedoBer <162101004+EscobedoBer@users.noreply.github.com>
2024-05-21 14:16:23 +0200

    DialogosEnProceso2

--- PracticaEntrega1/Assets/Scripts/Practica3Scripts/DialogueEnd.cs
+++ PracticaEntrega1/Assets/Scripts/Practica3Scripts/DialogueEnd.cs
@@ -1,13 +1,47 @@
-using System.Collections;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "DoNothing", menuName = "Dialogue/EndNode/DoNothing", order = 1)]
 public class EndNode : DialogueNode
 {
     public virtual void OnChosen(GameObject talker)
     {
+        // Implementación genérica o vacía
+    }
+}
+
+[CreateAssetMenu(fileName = "TheEndHappy", menuName = "Dialogue/EndNode/TheEndHappy", order = 0)]
+public class TheEndHappy : EndNode
+{
+    public override void OnChosen(GameObject talker)
+    {
         talker.GetComponent<MovementController>().Happy();
+    }
+}
+
+[CreateAssetMenu(fileName = "TheEndAngry", menuName = "Dialogue/EndNode/TheEndAngry", order = 1)]
+public class TheEndAngry : EndNode
+{
+    public override void OnChosen(GameObject talker)
+    {
+        talker.GetComponent<MovementController>().Angry();
+    }
+}
+
+[CreateAssetMenu(fileName = "TheEndVictory", menuName = "Dialogue/EndNode/TheEndVictory", order = 2)]
+public class TheEndVictory : EndNode
+{
+    public override void OnChosen(GameObject talker)
+    {
+        talker.GetComponent<MovementController>().Victory();
+    }
+}
+
+[CreateAssetMenu(fileName = "TheEndDefeat", menuName = "Dialogue/EndNode/TheEndDefeat", order = 3)]
+public class TheEndDefeat : EndNode
+{
+    public override void OnChosen(GameObject talker)
+    {
+        talker.GetComponent<MovementController>().Defeat();
     }
 }
--- PracticaEntrega1/Assets/Scripts/Practica3Scripts/MovementController.cs
+++ PracticaEntrega1/Assets/Scripts/Practica3Scripts/MovementController.cs
@@ -5,17 +5,61 @@ using UnityEngine;
 public class MovementController : MonoBehaviour
 {
     Animator animator;
+    float _timer;
+    bool isTheEnd; // Bandera para verificar si Happy() fue llamado
 
     void Start()
     {
         animator = GetComponent<Animator>();
+        _timer = 0f; // Inicializar _timer
+        isTheEnd = false; // Inicializar la bandera
+    }
 
-      
+    void Update()
+    {
+        if (isTheEnd)
+        {
+            _timer += Time.deltaTime; // Incrementar _timer con deltaTime
+
+            if (_timer > 2)
+            {
+                animator.SetBool("Happy", false);
+                animator.SetBool("Angry", false);
+                animator.SetBool("Victory", false);
+                animator.SetBool("Defeat", false);
+
+                isTheEnd = false; // Resetear la bandera
+                _timer = 0f; // Reiniciar el temporizador
+            }
+        }
     }
 
     public void Happy()
     {
         animator.SetBool("Happy", true);
-      
+
+        isTheEnd = true; // Activar la bandera
+        _timer = 0f; // Reiniciar el temporizador al llamar Happy()
+    }
+    public void Angry()
+    {
+        animator.SetBool("Angry", true);
+
+        isTheEnd = true; // Activar la bandera
+        _timer = 0f; // Reiniciar el temporizador al llamar Happy()
+    }
+    public void Victory()
+    {
+        animator.SetBool("Victory", true);
+
+        isTheEnd = true; // Activar la bandera
+        _timer = 0f; // Reiniciar el temporizador al llamar Happy()
+    }
+    public void Defeat()
+    {
+        animator.SetBool("Defeat", true);
+
+        isTheEnd = true; // Activar la bandera
+        _timer = 0f; // Reiniciar el temporizador al llamar Happy()
     }
 }
